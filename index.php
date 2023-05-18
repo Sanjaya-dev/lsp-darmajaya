ed9c81972f6a5494617c61915b69296a0eb27ca4
Fernanda <sanjaya.dev20@gmail.com>
2023-05-19 01:19:29 +0700

    memperbarui file index.php

--- index.php
+++ index.php
@@ -6,19 +6,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <title>Lembaga Sertifikasi Profesi Darmajaya</title>
     </head>
     <body>
+        <!-- header -->
         <header>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
+                <div class="container">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a class="navbar-brand" href="#">Hidden brand</a>
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <a class="navbar-brand logo" href="#"><img src="images/Logo-Darmajaya.png"></a>
+                        <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">HOME</a>
                             </li>
@@ -41,13 +43,69 @@
                     </div>
                 </div>
             </nav>
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="images/darmajaya_foto.jpg" class="d-block" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="10000">
+                        <img src="images/about_us.jpg" class="d-block" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </header>
+        <!-- contain -->
         <main>
-
+            
         </main>
         <footer>
+            <div class="container container_content">
+                <div class="row">
+                    <!-- addres -->
+                    <div class="col-lg-3 ">
+                    <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+                    <p>binarcarrental@gmail.com</p>
+                    <p>081-233-334-808</p>
+                    </div>
+                    <!-- navigation -->
+                    <div class="col-lg-3 nav_footer">
+                    <a href="#Our_Services">Our Services</a>
+                    <a href="#Why_Us">Why Us</a>
+                    <a href="#Testimonial">Testimonial</a>
+                    <a href="#FAQ">FAQ</a>
+                    </div>
+                    <!-- social media -->
+                    <div class="col-lg-3">
+                    <div class="sosmed">
+                        <p>Connect with us</p>
+                        <a href=""><img src="asset/icon_facebook.png" alt=""></a>
+                        <a href=""><img src="asset/icon_instagram.png" alt=""></a>
+                        <a href=""><img src="asset/icon_twitter.png" alt=""></a>
+                        <a href=""><img src="asset/icon_mail.png" alt=""></a>
+                        <a href=""><img src="asset/icon_twitch.png" alt=""></a>
+                    </div>
 
+                    </div>
+                    <div class="col-lg-3">
+                    <p>Copyright Binar 2022</p>
+                    <img src="asset/Rectangle 74.png">
+                    </div>
+                </div>
+            </div>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
